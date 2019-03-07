fac57e5a1284e838919bcc35703fd8a798db97ee
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-03-07 14:23:53 -0800

    Skeleton for slides.

---
+++ writing/nonFiction/talks/gdc2019/TheShapeOfSuccess/slides/shtmlToHtml.pl
@@ -0,0 +1,111 @@
+my $numArgs = $#ARGV + 1;
+
+if( $numArgs != 3 ) {
+    usage();
+    }
+
+
+my $listFileName = $ARGV[0];
+
+my $fileName = $ARGV[1];
+
+my $headName = $ARGV[2];
+
+
+# read entire file with <  > operator
+undef $/;
+
+
+open( LIST_FILE, "$listFileName" );    
+$listContents = <LIST_FILE>;
+close( LIST_FILE );
+
+open( SHTML_FILE, "$fileName" );    
+$shtmlContents = <SHTML_FILE>;
+close( SHTML_FILE );
+
+open( HEAD_FILE, "$headName" );    
+$headContents = <HEAD_FILE>;
+close( HEAD_FILE );
+
+open( FOOT_FILE, "foot".".html" );    
+$footContents = <FOOT_FILE>;
+close( FOOT_FILE );
+
+
+open( NAV_BAR_FILE, "navBar".".html" );    
+$navBarContents = <NAV_BAR_FILE>;
+close( NAV_BAR_FILE );
+
+
+
+$fileNumber = $fileName;
+
+# handle .wshtml case first
+$fileNumber =~ s/.wshtml//;
+
+$fileNumber =~ s/.shtml//;
+
+
+@listElements = split( /\s+/, $listContents );
+
+
+
+
+
+# for nav bar links
+$prevNumber;
+$nextNumber;
+
+for( $i=0; $i<=$#listElements; $i++ ) {
+
+	if( $listElements[$i] eq $fileNumber ) {
+		
+		if( $i == 0 ) {
+			# wrap back to last
+			$prevNumber = $listElements[ $#listElements ];
+		}
+		else { 
+			$prevNumber = $listElements[ $i - 1 ];
+		}
+
+		if( $i == $#listElements ) {
+			# wrap back to first
+			$nextNumber = $listElements[ 0 ];
+		}
+		else { 
+			$nextNumber = $listElements[ $i + 1 ];
+		}
+	}
+}
+
+
+$navBarContents =~ s/#PREV/$prevNumber/;
+$navBarContents =~ s/#NEXT/$nextNumber/;
+
+
+
+# correct for .wshtml files first
+$fileName =~ s/wshtml/html/;
+
+$fileName =~ s/shtml/html/;
+
+
+open( WRITE_FILE, ">$fileName" );    
+print WRITE_FILE $headContents;
+print WRITE_FILE $navBarContents;
+print WRITE_FILE $shtmlContents;
+print WRITE_FILE $footContents;
+close( WRITE_FILE );
+
+
+
+
+sub usage {
+    print "Usage:\n";
+    print "  shtmlToHtml.pl order_file_name shtml_file_to_process ".
+		"head_file_name\n";
+    print "Example:\n";
+    print "  shtmlToHtml.pl orderList.txt 0.shtml head.html\n";
+    die;
+    }
