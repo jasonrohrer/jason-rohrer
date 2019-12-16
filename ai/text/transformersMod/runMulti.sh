084f63191c3e0897d8592b7079aa59ffb87699a5
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-12-16 11:48:20 -0800

    Getting ready for a batch of 10K word samples with reseeding and no endoftext.

--- ai/text/transformersMod/runMulti.sh
+++ ai/text/transformersMod/runMulti.sh
@@ -7,6 +7,6 @@ do
 	seed=$(($i + $1))
 	echo "seed $seed"
 
-	python ./run_generation.py --model_type=gpt2 --length=20 --model_name_or_path=gpt2-xl --out_file=out${seed}_noEnd_noReseed.txt --in_file=sampleInputShort.txt --gen_words=5000 --seed=$seed --stop_token="<|endoftext|>"
+	python ./run_generation.py --model_type=gpt2 --length=20 --model_name_or_path=gpt2-xl --out_file=out${seed}_10000_noEnd_reseed.txt --in_file=sampleInputShort.txt --gen_words=10000 --seed=$seed --stop_token="<|endoftext|>"
 
 done
