7341ef9b6d63d38c58e58a5c37df792f3c6832c8
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-12-20 12:47:08 -0800

    Another unbuffer attempt.

--- ai/text/transformersMod/runBookBatch.sh
+++ ai/text/transformersMod/runBookBatch.sh
@@ -1,3 +1,3 @@
 rm outRunChapters.txt
 
-./runChapters.sh 2 10 42 earlSamsInput.txt 3 requiredWord.txt 2>&1 | tee outRunChapters.txt
+stdbuf -o0 ./runChapters.sh 2 10 42 earlSamsInput.txt 3 requiredWord.txt 2>&1 | tee outRunChapters.txt
