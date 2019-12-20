99d61e0a59b4c358955450bc9c9f338d486abba2
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-12-20 12:52:53 -0800

    Seems like stdbuf doesn't invoke scripts with bash for some reason.

--- ai/text/transformersMod/runBookBatch.sh
+++ ai/text/transformersMod/runBookBatch.sh
@@ -1,3 +1,3 @@
 rm outRunChapters.txt
 
-stdbuf -o0 ./runChapters.sh 2 10 42 earlSamsInput.txt 3 requiredWord.txt 2>&1 | tee outRunChapters.txt
+stdbuf -o0 bash ./runChapters.sh 2 10 42 earlSamsInput.txt 3 requiredWord.txt 2>&1 | tee outRunChapters.txt
