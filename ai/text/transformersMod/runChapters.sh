45e6f36e64c91d235524d4f40eea5989f5b702df
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-12-19 21:23:06 -0800

    Support for enforcing required word and min length in each chapter.

--- ai/text/transformersMod/runChapters.sh
+++ ai/text/transformersMod/runChapters.sh
@@ -1,5 +1,10 @@
-echo "Running $2 chapters, starting with chapter $1, numerical seed $3 and input text file $4, and generating $5 versions of each chapter"
+reqWord=`cat $6`
 
+echo "Running $2 chapters, starting with chapter $1, numerical seed $3 and input text file $4, and generating $5 versions of each chapter, requiring the word from file $6, '$reqWord'"
+
+
+minWords=3000
+maxWords=4500
 
 
 for (( c = 0; c < $2; c++ )) 
@@ -16,17 +21,30 @@ do
 		do
 			echo "Trying to generate Chapter $chapter (try $t) with seed $seed"
 			fileName=chapter${chapter}.${t}_${seed}_complete.txt
+
+			offTopicFileName=chapter${chapter}.${t}_${seed}_offTopic.txt
+
+			tooShortFileName=chapter${chapter}.${t}_${seed}_tooShort.txt
 			
 			overrunFileName=chapter${chapter}.${t}_${seed}_overrun.txt
 			
 			rm $fileName
 			rm $overrunFileName
 
-			python ./run_generation.py --model_type=gpt2 --length=20 --model_name_or_path=gpt2-xl --out_file=$overrunFileName --in_file=$4 --chapter_number=$chapter --gen_words=4000 --seed=$seed --stop_token="<|endoftext|>"
+			python ./run_generation.py --model_type=gpt2 --length=20 --model_name_or_path=gpt2-xl --out_file=$overrunFileName --in_file=$4 --chapter_number=$chapter --gen_words=$maxWords --seed=$seed --stop_token="<|endoftext|>"
 			
 			if grep -q "END OF CHAPTER" $overrunFileName; then
-				chapterDone="1"
-				mv $overrunFileName $fileName
+				
+				words=`wc -w $overrunFileName | cut -f1 -d' '`
+
+				if ! grep -q "$reqWord" $overrunFileName; then
+					mv $overrunFileName $offTopicFileName
+				elif [ "$words" -lt "$minWords" ]; then
+					mv $overrunFileName $tooShortFileName
+				else
+					chapterDone="1"
+					mv $overrunFileName $fileName
+				fi
 			fi
 			
 			seed=$((1 + $seed))
