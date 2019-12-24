944268a66b9f593d0bfab29248a657e777e8bb7c
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-12-23 19:25:09 -0800

    Support for eplilogue in join script.

--- ai/text/transformersMod/joinChapters.sh
+++ ai/text/transformersMod/joinChapters.sh
@@ -4,7 +4,7 @@
 
 while read -r line; 
 do 
-	cat $line | sed -e 's/ — /—/g' | sed -e "s/END OF CHAPTER/\f/" | sed -e '/^$/d' | sed -e 's/\(Chapter [0-9]*\)/\n\n\n\n\1\n\n/' | sed -e 's/Prologue/\n\n\n\n\Prologue\n\n/' 
+	cat $line | sed -e 's/ — /—/g' | sed -e "s/END OF CHAPTER/\f/" | sed -e '/^$/d' | sed -e 's/\(Chapter [0-9]*\)/\n\n\n\n\1\n\n/' | sed -e 's/Prologue/\n\n\n\nPrologue\n\n/' | sed -e 's/Epilogue/\n\n\n\nEpilogue\n\n/' 
 done
 
 echo -n -e "\n\n\n\nThe End\n"
