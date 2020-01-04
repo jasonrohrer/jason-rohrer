fd8a672bbed24df2aabc7c5ec27e241f57ed61eb
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-01-04 15:52:23 -0800

    Better trimming of blank lines.

--- ai/text/transformersMod/splitOzChapters.sh
+++ ai/text/transformersMod/splitOzChapters.sh
@@ -7,11 +7,20 @@ chapNum="1"
 
 outFile="ozChapters/$chapNum.txt"
 
+linesOut=0
+
 while read l; do
 	if [[ "$l" =~ ^[0-9]+. ]]; then
 		chapNum=$(($chapNum + 1))
 		outFile="ozChapters/$chapNum.txt"
 	else
-		echo "$l" >> $outFile
+		if [[ ! -z "$l" ]]; then
+			if [[ $linesOut -ne 0 ]]; then
+				echo "" >> $outFile
+				echo "" >> $outFile
+			fi	
+			echo -n "$l" >> $outFile
+			linesOut=1
+		fi
 	fi
 done <oz.txt
