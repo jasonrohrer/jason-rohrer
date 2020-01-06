47bd6aa73e53befecc5b4d8503cf57d45933af4a
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-01-06 12:09:32 -0800

    Word limit as parameters.

--- ai/text/transformersMod/runBookBatch.sh
+++ ai/text/transformersMod/runBookBatch.sh
@@ -1,3 +1,3 @@
 rm outRunChapters.txt
 
-unbuffer bash ./runChapters.sh 2 25 42 earlSamsInput.txt 1 requiredWord.txt --allow_chapter_sub_headings=0 2>&1 | tee outRunChapters.txt
+unbuffer bash ./runChapters.sh 2 25 42 earlSamsInput.txt 1 requiredWord.txt 2000 4000 --allow_chapter_sub_headings=0 2>&1 | tee outRunChapters.txt
