7f942494bfdd47e29b32574503e0fdad0600a07f
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-01-06 11:42:30 -0800

    Potter chapter splitter.

---
+++ ai/text/transformersMod/splitPotterChapters.sh
@@ -0,0 +1,26 @@
+
+rm -rf potterChapters
+
+mkdir potterChapters
+
+chapNum="1"
+
+outFile="potterChapters/$chapNum.txt"
+
+linesOut=0
+
+while read l; do
+	if [[ "$l" =~ ^[0-9]+. \ [A-Z\ ]+[A-Z\ ]+ ]]; then
+		chapNum=$(($chapNum + 1))
+		outFile="potterChapters/$chapNum.txt"
+	else
+		if [[ ! -z "$l" ]]; then
+			if [[ $linesOut -ne 0 ]]; then
+				echo "" >> $outFile
+				echo "" >> $outFile
+			fi	
+			echo -n "$l" >> $outFile
+			linesOut=1
+		fi
+	fi
+done <harryPotter.txt
