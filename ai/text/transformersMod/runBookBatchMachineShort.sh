be5103cac07c4a05917be4cc8d079957a3ce0b9d
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-01-22 10:39:34 -0800

    Whoops, only 17 chapters, starting with chapter 2.  Also extending limit to 3150 to allow for chapter 18 about mickey the cat.

--- ai/text/transformersMod/runBookBatchMachineShort.sh
+++ ai/text/transformersMod/runBookBatchMachineShort.sh
@@ -1,3 +1,3 @@
 rm outRunChapters.txt
 
-unbuffer bash ./runChapters.sh 2 18 42 earlSamsInput.txt 1 requiredWord.txt 2000 3000 --allow_chapter_sub_headings=0 2>&1 | tee outRunChapters.txt
+unbuffer bash ./runChapters.sh 2 17 42 earlSamsInput.txt 1 requiredWord.txt 2000 3150 --allow_chapter_sub_headings=0 2>&1 | tee outRunChapters.txt
