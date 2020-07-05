324f93986c003922c8eeb0aba9f64389d3d3777a
Jason Rohrer <jasonrohrer@fastmail.fm>
2020-07-05 09:48:15 -0700

    Whoops, messed up prefix measuring code.

--- ai/text/inferKitChatBot/index.php
+++ ai/text/inferKitChatBot/index.php
@@ -71,7 +71,7 @@ function cs_getMachineResponse() {
 
     global $chat_prefix;
 
-    $chatPlain = substr( $chatPlain, strlen( $chat_prefix ) + 10 );
+    $chatPlain = substr( $chatPlain, 1000 - ( strlen( $chat_prefix ) + 10 ) );
     
     // human's first line has \n\n in front
     $chatPlain = $chat_prefix . $chatPlain;
@@ -121,7 +121,7 @@ function cs_getMachineResponse() {
         
         
         $gennedChatLines =
-            preg_split('/$computerName:|Human:|Humans:|Machine:/', $textGen );
+            preg_split('/$computerName:|Human:|Humans:|The Human:|Machine:/', $textGen );
 
 
         $gennedLine = "";
