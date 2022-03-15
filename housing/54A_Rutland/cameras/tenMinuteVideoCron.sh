b5a15e6634e93a2aeb057d4eda68946fdf3f9772
Jason Rohrer <jasonrohrer@fastmail.fm>
2022-03-15 18:51:07 -0400

    Camera IP as a param.

--- housing/54A_Rutland/cameras/tenMinuteVideoCron.sh
+++ housing/54A_Rutland/cameras/tenMinuteVideoCron.sh
@@ -1,6 +1,7 @@
 # specify camera name (no spaces) as first argument
 cameraName=$1
-
+# specify camera IP as second argument
+cameraIP=$2
 
 basePath="/var/www/html/camera"
 
@@ -25,4 +26,4 @@ fileName="${dayName}_${hourMeridian}_${hourNum}_${minuteNum}.mp4"
 
 mkdir -p $path
 
-ffmpeg -rtsp_transport tcp -i rtsp://admin:@192.168.88.230:88/videoMain -c copy -an -t 600 $path$fileName
+ffmpeg -rtsp_transport tcp -i rtsp://admin:@$cameraIP:88/videoMain -c copy -an -t 600 $path$fileName
