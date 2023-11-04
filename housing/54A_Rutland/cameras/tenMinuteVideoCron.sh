a116fca99c10443b509ab64570a2a3751cfc778e
Jason Rohrer <jasonrohrer@fastmail.fm>
2023-11-04 16:49:46 -0400

    Newer cameras have passwords set.  Not sure why older ones do not.  Fixed all scripts to account for this.

--- housing/54A_Rutland/cameras/tenMinuteVideoCron.sh
+++ housing/54A_Rutland/cameras/tenMinuteVideoCron.sh
@@ -2,6 +2,8 @@
 cameraName=$1
 # specify camera IP as second argument
 cameraIP=$2
+# specify camera password as third argument (in quotes)
+pass=$3
 
 basePath="/var/www/html/camera"
 
@@ -26,4 +28,4 @@ fileName="${dayName}_${hourMeridian}_${hourNum}_${minuteNum}.mp4"
 
 mkdir -p $path
 
-ffmpeg -rtsp_transport tcp -i rtsp://admin:@$cameraIP:88/videoMain -c copy -an -t 600 $path$fileName
+ffmpeg -rtsp_transport tcp -i rtsp://admin:$pass@$cameraIP:88/videoMain -c copy -an -t 600 $path$fileName
