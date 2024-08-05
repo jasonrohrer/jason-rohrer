223a6e68cd5bd256a98c596c4b24a5bf241b7e8b
Jason Rohrer <jasonrohrer@fastmail.fm>
2024-08-05 14:15:25 +0000

    Index for remote camera web viewer.

---
+++ housing/54A_Rutland/cameras/remoteWebViewer/index.php
@@ -0,0 +1,16 @@
+
+<?php
+file_put_contents( "/home/jcr13/public_html/flag.txt", "1" );
+
+?>
+
+
+
+<img src=pics/backDoor.jpg><br><br><br>
+<img src=pics/frontDoor.jpg><br><br><br>
+<img src=pics/frontYard.jpg><br><br><br>
+<img src=pics/rearDrive.jpg><br><br><br>
+<img src=pics/bulkhead.jpg><br><br><br>
+<img src=pics/frontDrive.jpg><br><br><br>
+<img src=pics/garage.jpg><br><br><br>
+<img src=pics/sideDrive.jpg><br><br><br>
