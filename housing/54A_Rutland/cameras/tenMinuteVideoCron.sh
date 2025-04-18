b7b07dd85792e1476543d824f976a78691259b47
Jason Rohrer <jasonrohrer@fastmail.fm>
2025-04-17 20:32:06 -0400

    Added support to video-recording script to accept one extra parameter (optional), 1 or 0, to record audio.  Only some cameras seem to support the audio stream.  Not sure why.

--- housing/54A_Rutland/cameras/tenMinuteVideoCron.sh
+++ housing/54A_Rutland/cameras/tenMinuteVideoCron.sh
@@ -5,6 +5,17 @@ cameraIP=$2
 # specify camera password as third argument (in quotes)
 pass=$3
 
+ 
+audio=0
+
+if [ $# -eq 4 ]
+then
+	audio=$4
+fi
+
+
+
+
 basePath="/var/www/html/camera"
 
 
@@ -28,4 +39,46 @@ fileName="${dayName}_${hourMeridian}_${hourNum}_${minuteNum}.mp4"
 
 mkdir -p $path
 
-ffmpeg -rtsp_transport tcp -i rtsp://admin:$pass@$cameraIP:88/videoMain -c copy -an -t 600 $path$fileName
+if [ $audio -eq 1 ]
+then
+	# include audio stream, re-encode as aac
+	ffmpeg -rtsp_transport tcp -i rtsp://admin:$pass@$cameraIP:88/videoMain -c:v copy -c:a aac -t 600 $path$fileName
+else
+	# skip audio
+	ffmpeg -rtsp_transport tcp -i rtsp://admin:$pass@$cameraIP:88/videoMain -c copy -an -t 600 $path$fileName
+fi
+
+
+
+# NOTE
+# camera video streams SHOULD include audio, but this seems to be stalled
+# on many of the cameras (both streams are announced, but only the video
+# stream is transmitted by the camera).
+
+# audio streams seem function ONLY for these old cameras (original to house)
+#
+# frontDoor 192.168.88.230
+# backDoor 192.168.88.231
+# garage 192.168.88.232
+#
+# AND this one new camera:
+#
+# sideYardRear 192.168.88.182
+#
+# the -an param above skips the audio stream, allowing the video to record
+#
+# HOWEVER
+# For a camera that IS streaming audio, the following ffmpeg command works
+# to record both audio and video:
+#
+# ffmpeg -rtsp_transport tcp -i rtsp://admin:@192.168.88.231:88/videoMain -c:v copy -c:a aac -t 5 out.mp4
+#
+# Audio alone can be recorded thus:
+#
+# ffmpeg -rtsp_transport tcp -i rtsp://admin:@192.168.88.231:88/audio -c copy -t 5 out.wav
+
+# In future, when we install an outdoor microphone, we can connect the audio
+# to one of the cameras above that supports streaming audio and video.
+#
+# Then, we can update the userCronTab to take 1 as a 4th parameter for
+# the camera that has the microphone.
