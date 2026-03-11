bb5d01529568e6c5c23f022bf2996380d4374dbf
Jason Rohrer <jasonrohrer@fastmail.fm>
2026-03-11 18:02:45 -0400

    Fixed bash-specific compare

--- housing/54A_Rutland/cameras/periodicSnapCron.sh
+++ housing/54A_Rutland/cameras/periodicSnapCron.sh
@@ -38,7 +38,7 @@ fileName="${hourNum}_${minuteNum}_$secondsNum.jpg"
 
 camURL="http://$cameraIP:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=admin&pwd=$pass"
 
-if [[ "$cameraIP" == "192.168.88.146" ]];
+if [ "$cameraIP" = "192.168.88.146" ];
 then
 	# special case for LPR camera
 	camURL="http://admin:$pass@$cameraIP/cgi-bin/snapshot.cgi"
